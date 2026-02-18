ebc054b30427866793c048f12d49edbab2cc7c90
jurayevmirjalol054-pixel <jurayevmirjalol054@gmail.com>
2026-02-18 15:08:01 +0500

    Update Main.py
    1

--- Main.py
+++ Main.py
@@ -1,1 +1,12 @@
+railway app/ 
+  main.py
+  requirements.txt
 
+  main.pay
+  from flask import Flask
+app=Flask(_name_)
+
+@app.route("/")
+def home():
+  return "salom!  Bu registratsiyasiz Paas amaliy ishi.
+  app.run(host="0.0.0.0,port=8080
